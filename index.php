7cb68fc3e91a908278ef98204ded168926d20bf5
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-05-13 21:20:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GoRent</title>
+    <link rel="shortcut icon" href="src/images/go rent.png" type="image/x-icon">
+    
+    <!--Link CSS-->
+    <link rel="stylesheet" href="src/css/Home.CSS">
+    <!--LInk boostrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Boostrap icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body class="d-flex flex-column min-vh-100">
+
+    <div class="container-fluid" style="background-color: black;">
+       <center> <img id="logoimgs" src="src/images/go rent.png"></center>
+    </div>
+    
+    <div class="topnav container-fluid"  >
+    <a class="active" href="index.html" style="margin-left:400px">Home</a>
+    <a  href="card.html">Card</a>
+    <a href="contact.html">Contacts</a>
+    <a href="Login.html" style="margin-left:800px">Log in</a>
+    <a href="Registation.html" >Sign Up</a>
+    </div>
+
+
+ <!-- Footer -->
+ <footer class="bg-white mt-auto footer"  id="footer">
+    <div class="container py-5">
+      <div class="row py-4">
+        <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="src/images/logo1.png" alt="" width="auto" height="80px" class="mb-3">
+        
+          <ul class="list-inline mt-4">
+            <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter" style="color:lavender;"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook" style="color:lavender;"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram" style="color:lavender;"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fa fa-linkedin" style="color:lavender;"></i></a></li>
+            
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">Quick Links</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" class="flinks">Home</a></li>
+            <li class="mb-2"><a href="#" class="flinks">Login Men</a></li>
+            <li class="mb-2"><a href="#" class="flinks">Registation</a></li>
+            <li class="mb-2"><a href="#" class="flinks">Payments</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">Company</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" class="flinks">Login</a></li>
+            <li class="mb-2"><a href="#" class="flinks">Register</a></li>
+            <li class="mb-2"><a href="#" class="flinks">Wishlist</a></li>
+            <li class="mb-2"><a href="#" class="flinks">Our Products</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- Copyrights -->
+    <div class="bg-light py-4">
+      <div class="container text-center">
+        <p class="text-muted mb-0 py-2">All rights reserved by GoRent pvt(lmt)</p>
+      </div>
+    </div>
+  </footer>
+  <!-- End -->
+    <!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html> 
